40b1852ee55e359727ec8e537f8ac76224292751
Katie  Lin <Katie@Katies-MacBook-Air.local>
2015-10-28 14:33:25 -0700

    Advanced check answer feature

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,9 @@
 class AnswersController < ApplicationController
+	def index
+		@answers = Answer.where(puzzle_id: :id)
+		
+	end
+
 	def new
 		@answer = Answer.new
 		@puzzle = Puzzle.find(1)
@@ -21,7 +26,7 @@ class AnswersController < ApplicationController
 		@answer = Answer.find(params[:id])
 
 		if check_answer(@answer, @puzzle)
-			@message = "Congratulations! Your answer is correct =P"
+			@message = "Congratulations! Your answer is correct!"
 		else
 			@message = "Sorry, you were not correct."
 		end
@@ -36,40 +41,53 @@ class AnswersController < ApplicationController
 		#check each word in the array. If all the keywords were found in the user's answer, the answer is correct
 		def check_answer(answer, puzzle)
 
-			arr = answer.text.downcase.split(' ')
-			keyword_check = {}
-			keywords = Keyword.where(puzzle_id: puzzle.id)
+			# arr = answer.text.downcase.split(' ')
+			# keyword_check = {}
+			# keywords = Keyword.where(puzzle_id: puzzle.id)
 
-			#create a new hash that assigns each keyword as the key and value is initialized as false
-			keywords.each do |x|
-				keyword_check[x.text] = false
-			end
+			# #create a new hash that assigns each keyword as the key and value is initialized as false
+			# keywords.each do |x|
+			# 	keyword_check[x.text] = false
+			# end
 
-			#if a keyword is found in the answer, it's value is set to true
-			arr.each do |x|
-				keywords.each do |y|
-					if x.eql? y.text
-						keyword_check[x] = true
-					end
-				end
-			end
+			# #if a keyword is found in the answer, it's value is set to true
+			# arr.each do |x|
+			# 	keywords.each do |y|
+			# 		byebug
+			# 		if x.eql? y.text or x.include? y.text then
+			# 			keyword_check[x] = true
+			# 		end
+			# 	end
+			# end
+			# # byebug
+
+			# #check if all the values in the hash are true
+			# all_values_true = true
 			# byebug
+			# keywords.each do |x|
+			# 	if keyword_check[x.text] == false
+			# 		all_values_true = false
+			# 	end
+			# end
 
-			#check if all the values in the hash are true
-			all_values_true = true
+			# #if all the key values are true, all keywords have been matched
+			# if all_values_true == true
+			# 	return true
+			# else
+			# 	return false
+			# end
+
+
+			keywords = Keyword.where(puzzle_id: puzzle.id)
+			keyword_arr = []
+
 			keywords.each do |x|
-				if keyword_check[x.text] == false
-					all_values_true = false
-				end
+				keyword_arr.push(x.text)
 			end
 
-			#if all the key values are true, all keywords have been matched
-			if all_values_true == true
-				return true
-			else
-				return false
-			end
 
+			answer = answer.text
+			keyword_arr.all? {|word| answer.downcase.match(word) }
 
 		end
 
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
 
   get '/puzzles/:id', to: 'puzzles#show'
   post '/puzzles/:id/answers', to: 'answers#create', as: "puzzle_answers"
+  get '/puzzles/:id/answers/index', to: 'answers#index'
 
 end
