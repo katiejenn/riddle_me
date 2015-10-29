c1a7dcc889f678455038be22981962216e26b38f
Katie  Lin <Katie@Katies-MacBook-Air.local>
2015-10-28 20:49:56 -0700

    sign up and login working

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+	def index
+    	@users = User.all
+    	render :index
+  	end
+
+  	def new
+	    @user = User.new
+	    render :new
+  	end
+
+  	def create
+	    user_params = params.require(:user).permit(:email, :password)
+	    is_admin = params[:is_admin]
+
+	    @user = User.create(user_params)
+	    # byebug
+	    if is_admin == '1'
+	    	@user.is_admin = true
+	    else
+	    	@user.is_admin = false
+	    end
+
+	    @user.save
+	    redirect_to "/users/#{@user.id}"
+  	end
+
+  	def show
+    	@user = User.find(params[:id])
+    	@solved_puzzles = 
+    	render :show
+  	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
 
   get '/', to:'welcome#home'
 
+  get "/users", to: "users#index", as: "users"
+  get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create"
+  get "/users/:id", to: "users#show"
+
   get '/answers/new', to: 'answers#new'
   # post '/answers', to: 'answers#create'
   get 'answers/:id', to: 'answers#show'
 
   get '/puzzles/:id', to: 'puzzles#show'
   post '/puzzles/:id/answers', to: 'answers#create', as: "puzzle_answers"
-  get '/puzzles/:id/answers/index', to: 'answers#index'
+  get '/puzzles/:id/answers', to: 'answers#index'
 
 end
